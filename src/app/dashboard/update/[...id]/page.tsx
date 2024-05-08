59883b8e0f61cd7845b149f39250802ef10bbee9
Seamey <channtha.seamey.1221@rupp.edu.kh>
2024-05-08 12:20:59 +0700

    done

--- src/app/dashboard/update/[...id]/page.tsx
+++ src/app/dashboard/update/[...id]/page.tsx
@@ -2,12 +2,6 @@ import { baseApi } from '@/lib/constants/BaseURL'
 import UpdateProductForm from '@/components/form/FormUpdate'
 import React from 'react'
 import { Products } from '@/types/product'
-import { useCreateProductMutation,
-  useUpdateProductMutation,
-useDeleteProductMutation,
-useGetProductByIdQuery,
-useGetProductBySellerQuery,
-useGetProductsQuery } from '@/redux/service/product'
 
 type Props = {
   params:{
@@ -15,31 +9,28 @@ type Props = {
   },
   
 }
-// const getData=async(props:Products)=>
-// {
-//   const data=await fetch(`${baseApi}products/${props.id}/`)
-//   const respone= await data.json()
-//   console.log(respone)
-//   return respone.results;
-// }
-export default async function page({params}:Props) {
-  // const data= await getData(props.id)
-  const id = params.id
-  const getById = useGetProductByIdQuery(id)
-  console.log(getById)
-  
+const getData=async(props:Products)=>
+{
+  const data=await fetch(`${baseApi}products/${props.id}/`)
+  const respone= await data.json()
+  console.log(respone)
+  return respone.results;
+}
+export default async function page(props:any) {
+  const data= await getData(props.id)
+  console.log(data)
   return (
   <>
-      {/* <UpdateProductForm 
-        category={getById.category.name}
-        price={getById.price}
+      <UpdateProductForm 
+        category={data.category.name}
+        price={data.price}
         desc={data.desc}
         name={data.name}
         quantity={data.quantity}
         image={data.image}
         id={data.id}
         seller={data.seller}
-      /> */}
+      />
   </>
   )
 }
